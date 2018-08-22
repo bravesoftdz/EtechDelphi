574ffbd4819b6eef482e25b96c238269a3fd30a5
WillCruz <lynkan@hotmail.com>
2018-08-21 23:37:45 -0300

    commitando parte inicial do projeto, tela e menu split view

---
+++ Etech.dpr
@@ -0,0 +1,14 @@
+program Etech;
+
+uses
+  Vcl.Forms,
+  Principal in 'views\Principal.pas' {FrmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFrmPrincipal, FrmPrincipal);
+  Application.Run;
+end.
